28c1ad2308c8ff337b6f396db711afd95f47c01c
4forces <62040770+4forces@users.noreply.github.com>
2021-04-14 22:48:22 +0800

    Done requests_http. Tidy up code formatting

--- scrape.py
+++ scrape.py
@@ -2,32 +2,42 @@
 import rpa
 import os
 import requests
-from bs4 import BeautifulSoup as soup
-# import selenium as s
-# import pandas as p
+import bs4
+import selenium as s
+import pandas as p
 # from urllib.request import Request, urlopen
-# from bs4 import BeautifulSoup  as soup
 
-
+# ----- Requests HTTP ----- #
 url = 'https://www.singaporetech.edu.sg/undergraduate-programmes/engineering'
 
-# --- badurl e.g. --- #
+# -- badurl example -- #
 # badurl ='https://www.singaporetech.edu.sg/undergraduate-programmes/engineerin'
 # bad_r = requests.get(badurl)
 # print(bad_r.raise_for_status())
 
-r = requests.get(url)
-print(r, '\n ---')
-print(r.text[:300])
-# wb = write binary to avoid unicode encode issues
-playfile = open('testFile.txt','wb')
-# writes url:'r' contents to playfile in bytes of 100000
-for chunk in r.iter_content(100000):
-    playfile.write(chunk)
-playfile.close()
+def request_http()
+    print('<----- START OF HTTP REQUESTS OUTPUT ----->')
+    # r = requests.get(url)
+    # print(r, '\n ---')
+    # print(r.text[:300])
+    #
+    # # wb: write binary to avoid unicode encode issues
+    # playfile = open('testFile.txt','wb')
+    #
+    # # writes url:'r' contents to 'playfile' in bytes of 100000
+    # for chunk in r.iter_content(100000):
+    #     playfile.write(chunk)
+    # playfile.close()
+    print('<----- END OF HTTP REQUESTS OUTPUT ----->')
 
 
+print('<----- START OF BeautifulSoup OUTPUT ----->')
 # --- from https://www.bestproxyreviews.com/scrapy-vs-selenium-vs-beautifulsoup-for-web-scraping/ --- #
 # r = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 # webpage = urlopen(r).read()
 # print(webpage)
+
+
+# ---- BeautifulSoup Parsing ---- #
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+print('<----- END OF BeautifulSoup OUTPUT ----->')
